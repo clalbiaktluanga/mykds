d43e94aec44effe89e2a458eed14cd3beef785e0
lchhangte <lbtachhangte@gmail.com>
2026-04-11 13:39:29 +0530

    Added vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,12 +2,16 @@ import { getServerSession } from 'next-auth';
 import Navbar from '@/components/Navbar';
 import Providers from './providers';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export default async function RootLayout({ children }) {
   const session = await getServerSession();
   return (
     <html lang="en">
-      <body>
+        <head>
+          <Analytics />
+        </head>
+        <body>
         <Providers>
           {session && <Navbar role={session.user?.role} name={session.user?.name} />}
           <main style={{ paddingTop: session ? '60px' : 0 }}>
